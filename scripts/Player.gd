fa85a860f43e27a8941b416f075496b549b78fba
Käselord <emil@richter-online.eu>
2024-05-09 22:40:43 +0200

    tweaked player movement
    coyote time, jump buffer, DON'T USE INBUILT GRAVITY(!!!!), simplified movement

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -2,11 +2,14 @@ extends CharacterBody3D
 
 @onready var Ray = $Camera3D/RayCast3D
 
-const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
+const WALKING_SPEED : float = 3.0
+const JUMP_VELOCITY : float = 6.0
+const ACCEL : float = 0.35
+const GRAVITY : float = 15.0
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+var input_dir : Vector2 = Vector2(0, 0)
+var jump_input_buffer : int = 0
+var coyote_buffer : int = 0
 
 func _unhandled_input(event):
 	if event is InputEventMouseButton:
@@ -26,7 +29,7 @@ func _unhandled_input(event):
 		
 		if event.is_action_pressed("break"):
 			collider.set_cell_item(
-				collider.map_to_local(
+				collider.map_to_local(# Handle jump.
 					Ray.get_collision_point() - Ray.get_collision_normal()
 					),
 				-1
@@ -42,23 +45,41 @@ func _unhandled_input(event):
 			)
 
 func _physics_process(delta):
-	# Add the gravity.
+	# gravity
 	if not is_on_floor():
-		velocity.y -= gravity * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir = Input.get_vector("left", "right", "up", "down")
+		velocity.y -= GRAVITY * delta
+	
+	# tick down all the buffers by one frame
+	if coyote_buffer > 0:
+			coyote_buffer -= 1
+	
+	if jump_input_buffer > 0:
+		jump_input_buffer -= 1
+	
+	# get movement dir
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
-
+	# interpolate velocity towards movement dir * speed
+	velocity.x = lerp(velocity.x, direction.x * WALKING_SPEED, ACCEL)
+	velocity.z = lerp(velocity.z, direction.z * WALKING_SPEED, ACCEL)
 	move_and_slide()
+
+func _process(_delta):
+	input_dir = Input.get_vector("left", "right", "up", "down")
+	if Input.is_action_just_pressed("jump"):
+		# 7 frames of buffer time to hit jump early before hitting the floor
+		jump_input_buffer = 7
+		
+		# check if player was on floor within the last 7 frames, if yes, jump
+		if coyote_buffer > 0:
+			jump()
+	
+	if is_on_floor():
+		coyote_buffer = 7
+		# check if jump has been pressed within the last 7 frames, if yes, jump
+		if jump_input_buffer > 0:
+			jump()
+
+func jump():
+	velocity.y = JUMP_VELOCITY
+	coyote_buffer = 0
+	jump_input_buffer = 0
--- scripts/menus/Loading.gd
+++ scripts/menus/Loading.gd
@@ -8,7 +8,7 @@ func _ready():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
 	var progress = []
 
 	var status = ResourceLoader.load_threaded_get_status(targetScene, progress)
