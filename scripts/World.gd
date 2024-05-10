0d7da1f372c6251e9eeb66c7a9f4f48d59604b02
cyteon <cyteon>
2024-05-10 13:31:39 +0200

    Some changes to diffrent files, fix mod support and change to use zips

--- scripts/World.gd
+++ scripts/World.gd
@@ -1,6 +1,5 @@
 extends Node3D
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -8,10 +7,9 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
-	pass
+	$CanvasLayer/FPSCounter.text = "FPS: %s" % Engine.get_frames_per_second()
 
 
 func _on_heightmap_generator_3d_chunk_generation_finished(chunk_position):
 	$CanvasLayer/CreatingWorld.hide()
-	
 	# TODO: Make progressbar do something
--- scripts/autoloads/ModLoader.gd
+++ scripts/autoloads/ModLoader.gd
@@ -1,10 +1,11 @@
 extends Node
 
-# TODO: so uh better mod support?
+# TODO: Better mod support
 
 # Called when the node enters the scene tree for the first time.
 
 var modPath = OS.get_executable_path().get_base_dir() + "/mods/"
+var modExtension = ".zip"
 
 func _ready():
 	
@@ -12,7 +13,9 @@ func _ready():
 		print("Mods folder found at %s" % modPath)
 		
 		for file in DirAccess.get_files_at(modPath):
-			if file.ends_with(".pck"):
+			if file.ends_with(modExtension):
+				print("----------")
+				
 				print("Loading mod %s" % file)
 				
 				var success = ProjectSettings.load_resource_pack(modPath + file)
@@ -21,21 +24,25 @@ func _ready():
 				
 				if success:
 					# FIXME: Not printing mod folder even tough its in the pck that was exported
-					print(DirAccess.get_directories_at(""))
 					
 					var config = ConfigFile.new()
 
 					# Load data from a file.
-					print("Loading manifest: res://mod/%s/manifest.cfg" % file.replace(".pck", ""))
-					var err = config.load("res://mod/%s/manifest.cfg" % file.replace(".pck", ""))
+					print("Loading manifest: res://mod/%s/manifest.cfg" % file.replace(modExtension, ""))
+					var err = config.load("res://mod/%s/manifest.cfg" % file.replace(modExtension, ""))
 					
-					print(config)
 					
 					var author = config.get_value("mod", "author")
 					var modName = config.get_value("mod", "name")
 					var version = config.get_value("mod", "version")
 					
 					print("Author: %s, Name: %s, Version: %s" % [author, modName, version])
+					
+					var modFile = load("res://mod/%s/mod.gd" % file.replace(modExtension, ""))
+					
+					print("----------")
+					
+					modFile.ready()
 				
 
 
